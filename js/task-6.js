4e19647244aea1fd9956e7f6cc3124a6e93e3ddd
Alexandr Grib <alexgribaldi@gmail.com>
2020-09-20 15:41:46 +0200

    добавляет задану №6

--- js/task-6.js
+++ js/task-6.js
@@ -8,3 +8,16 @@
 
 // let input;
 // let total = 0;
+
+let input;
+let total = 0;
+
+do {
+  input = prompt("Введите число");
+  if (isNaN(input)) {
+    alert("Было введено не число, попробуйте ещё раз!");
+  } else {
+    total += Number(input);
+  }
+} while (input !== null);
+alert(`Общая сумма введенных чисел ${total}`);
